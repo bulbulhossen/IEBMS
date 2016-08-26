54b0e1db7b1c8304c974e230c0bfdf14ce6bec9d
bulbulhossen <info247.bulbul@gmail.com>
2016-08-26 17:39:18 +0600

    Initial commit

---
+++ app/src/main/java/com/bulbulhossen/billcalculator/MainActivity.java
@@ -0,0 +1,46 @@
+package com.bulbulhossen.billcalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText firstNumber;
+    EditText secondNumber;
+    TextView addResult;
+    Button btnAdd;
+
+    double num1,num2,sum;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstNumber = (EditText)findViewById(R.id.txtNumber1);
+        secondNumber = (EditText)findViewById(R.id.txtNumber2);
+        addResult = (TextView)findViewById(R.id.txtResult);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(firstNumber.getText().toString());
+                num2 = Double.parseDouble(secondNumber.getText().toString());
+                sum = num1 - num2;
+                addResult.setText(Double.toString(sum));
+            }
+        });
+
+
+
+    }
+}
